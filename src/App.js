d34047b8b8890098900c7d0b7e4e9d9d948e7826
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-07-03 15:28:58 +0200

    Configure Routes in app and index

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./routes";
 const App = () => {
-  return <></>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/index.html" element={<Home />} />
+      </Routes>
+    </Layout>
+  );
 };
 
 export default App;
