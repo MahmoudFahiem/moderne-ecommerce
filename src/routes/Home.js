a5af14866f1ea8193926b91d22ad0e75b58eb517
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-07-03 15:27:57 +0200

    Add routes

---
+++ src/routes/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import Slider from "react-slick";
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Home;
