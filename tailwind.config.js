370c8cf9a6a086fd7709ab9dee8f2d8be05d02fc
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-07-03 15:31:07 +0200

    Configure daisyUI package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,51 @@
 /** @type {import('tailwindcss').Config} */
+const colors = {
+  primary: "#D37657",
+  secondary: "#484543",
+  accent: "#1FB2A6",
+  topbar: "#73A9A9",
+  neutral: "#191D24",
+  "base-100": "#fff",
+  info: "#3ABFF8",
+  success: "#5FBD74",
+  warning: "#FBBD23",
+  error: "#F87272",
+  flag: "#2E58B2",
+  white: "#fff",
+  black: "#000",
+};
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    // container: {
-    //   center: true,
-    //   padding: "1rem",
-    // },
-    // screens: {
-    //   sm: "568px",
-    //   md: "768px",
-    //   lg: "992px",
-    //   xl: "1200px",
-    //   "2lx": "1400px",
-    // },
-    // colors: {
-    //   primary: "#D37657",
-    //   secondary: "#484543",
-    //   success: "#5FBD74",
-    //   topbar: "#73A9A9",
-    //   flag: "#2E58B2",
-    // },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    screens: {
+      sm: "568px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      "2lx": "1400px",
+    },
+    colors: { ...colors },
+    fontFamily: {
+      sans: ["poppins", "ui-sans-serif", "sans-serif"],
+    },
     extend: {
-      // fontFamily: {
-      //   poppins: "font-family: 'Poppins', sans-serif",
-      // },
+      fontFamily: {
+        poppins: "font-family: 'Poppins', sans-serif",
+      },
     },
   },
-  plugins: [],
+  safelist: ["opacity-0", "opacity-100"],
+  daisyui: {
+    themes: [
+      {
+        base: { ...colors },
+      },
+    ],
+  },
+  plugins: [require("daisyui"), require("@tailwindcss/typography")],
 };
