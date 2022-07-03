a01fdd696b24bcae27232dfcfa7198cd37e07260
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-07-03 15:33:19 +0200

    Create Topbar Component

---
+++ src/components/Layout/Topbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Topbar = () => {
+  return (
+    <div className="bg-topbar text-center py-4">
+      <p className="uppercase text-white">FREE SHIPPING ON ALL U.S. ORDERS $500+
+      </p>
+    </div>
+  );
+};
+
+export default Topbar;
